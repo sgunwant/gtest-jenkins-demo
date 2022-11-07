22798e6e020da0a2efcecfffcbb31161413bde89
sgunwant <shivagunwant@coredge.io>
2022-11-07 20:38:30 +0000

    Initial commit for demo

---
+++ src/Factorial.h
@@ -0,0 +1,13 @@
+class Factorial {
+public:
+static unsigned long long GetFactorial(int input);
+};
+
+/*class Factorial {
+public:
+static unsigned long long GetFactorial(int input);
+};class Factorial {
+public:
+static unsigned long long GetFactorial(int input);
+};*/
+
